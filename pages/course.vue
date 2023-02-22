163f16378821a341ca0d61de6445a0816f0dc38f
Edwin Boon <edwin.boon@eo.nl>
2023-02-22 13:22:55 +0100

    Ready to deploy

--- pages/course.vue
+++ pages/course.vue
@@ -1,7 +1,6 @@
 <script setup>
-  const { chapters } = useCourse()
+const { chapters } = useCourse();
 </script>
-
 
 <template>
   <div
@@ -21,17 +20,23 @@
         class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] max-w-[30ch] flex flex-col"
       >
         <h3>Chapters</h3>
-        <div class="space-y-1 mb-4 flex flex-col" v-for="chapter in chapters" :key="chapter.slug">
-          <h4>{{  chapter.title  }}</h4>
+        <div
+          class="space-y-1 mb-4 flex flex-col"
+          v-for="chapter in chapters"
+          :key="chapter.slug"
+        >
+          <h4>{{ chapter.title }}</h4>
           <NuxtLink
             v-for="(lesson, index) in chapter.lessons"
             :key="lesson.slug"
             class="flex flex-row space-x-1 no-underline prose-sm font-normal py-1 px-4 -mx-4"
-            :to="`/course/chapter/${chapter.slug}/lesson/${lesson.slug}`"
+            :to="lesson.path"
+            :class="{
+              'text-blue-500': lesson.path === $route.fullPath,
+              'text-gray-600': lesson.path !== $route.fullPath
+            }"
           >
-            <span class="text-gray-500"
-              >{{ index + 1 }}.</span
-            >
+            <span class="text-gray-500">{{ index + 1 }}.</span>
             <span>{{ lesson.title }}</span>
           </NuxtLink>
         </div>
