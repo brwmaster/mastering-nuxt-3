1e23931c3a52b5a3197289ac25da58227c017c8b
Edwin Boon <edwin.boon@eo.nl>
2023-02-28 14:30:00 +0100

    We can now login with Github, also created a user-card component to display the username and profile image

---
+++ components/UserCard.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const user = useSupabaseUser();
+
+const name = computed(() => user?.value.user_metadata.full_name);
+const profile = computed(() => user?.value.user_metadata.avatar_url);
+</script>
+
+<template>
+  <div v-if="user" class="rounded p-3 flex items-center space-x-3 bg-white">
+    <img
+      :src="profile"
+      alt="Profile image of the logged in user."
+      class="rounded-full w-12 h-12 border-2 border-blue-400"
+    />
+    <div class="text-right">
+      <div class="font-medium">{{ name }}</div>
+      <button class="text-sm underline text-slate-500">Logout</button>
+    </div>
+  </div>
+</template>
